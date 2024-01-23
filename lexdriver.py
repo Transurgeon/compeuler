886d64a3e7992e4346c82e37aa8380347ae9ec30
Transurgeon <peter.zijie@gmail.com>
2024-01-22 21:24:02 -0500

    adding files and more changes to the driver

--- lexdriver.py
+++ lexdriver.py
@@ -1,32 +1,39 @@
 
 class Token:
-    def __init__(self, type, lex, location):
+    def __init__(self, type, lex, location: int):
         self.type = type
         self.lexeme = lex
         self.location = location
-
+    
+    def __str__(self) -> str:
+        return self.lexeme + str(self.location)
 
 class LexFridman:
-    def __init__(self, text):
+    def __init__(self, text: str):
         self.text = text
         self.currentChar = ""
         self.currentIdx = 0
         self.nextChar = ""
         self.nextIdx = 1
         self.errors = []
+        self.tokens = []
         self.line = 0
         
     
     def peek(self):
         list = []
-    def checkToken(self):
-        list = []
+    def nextToken(self):
+        if self.currentChar == ' ':
+            return
+        
     def validateToken(self):
         list = []
     
     
-    
+lexemes = {1 : 'id', 2: 'alphanum', 3: 'integer', 4: 'float', 5: 'fraction', 6: 'letter', 7: 'digit', 8: 'nonzero'}  
 f = open("assignment1/lexnegativegrading.src")
 text = f.read()
 lex = LexFridman(text)
 print(lex.text)
+tok = Token("hi", "hi", 2)
+print(tok)
