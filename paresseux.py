ac46d4686af23f7d6cee4580ecbd462cfc04ee75
Transurgeon <peter.zijie@gmail.com>
2024-02-10 22:42:43 -0500

    finishing LL(1) grammar for parser of A2

---
+++ paresseux.py
@@ -0,0 +1,16 @@
+class Paresseux:
+    def __init__(self) -> None:
+        self.currToken = []
+    
+    def checkToken(self, type):
+        pass
+    
+    def checkNext(self, type):
+        pass
+    
+    def matchToken(self, type):
+        pass
+    
+    def nextToken(self, type):
+        pass
+    
